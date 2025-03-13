c94fee8776ae3922c4283faa9937350c004dd947
Fouad ELAYACHI <fouad.elayachi@oracle.com>
2025-03-13 14:00:25 +0000

    Initial commit

---
+++ tests/test_math_operations.py
@@ -0,0 +1,12 @@
+# tests/test_math_operations.py
+import unittest
+from src.math_operations import add
+
+class TestMathOperations(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
+
